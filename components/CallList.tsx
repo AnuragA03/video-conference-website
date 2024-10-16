726aeb511ff1d4a4c0ca8473d5a613b7e589325a
AnuragA03 <anuragadhikari2003@gmail.com>
2024-10-16 15:29:23 +0530

    Changes for deployment

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -53,6 +53,7 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
         setRecordings(recordings);
       
       } catch (error) {
+        console.log(error)
         toast({title: 'Try again later'})
       }
     }
